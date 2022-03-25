2eb6efcade0a33560c0c6d80f9eb08b01a2c8841
Amir <amirhatam136@gmail.com>
2022-03-25 07:33:13 +0100

    add background color for the pages calculator & games

--- src/views/Calculators.jsx
+++ src/views/Calculators.jsx
@@ -3,58 +3,60 @@ import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardLink, MDBContai
 
 export default function Calculator() {
     return (
-        <MDBContainer className='pb-md-5'>
-            <MDBRow >
-                <MDBCardTitle className='text-center my-md-5 display-4'>The Calculators</MDBCardTitle>
-                <MDBCol md='4'>
-                    <MDBCard >
-                        <MDBCardBody>
-                            <MDBCardTitle className='text-uppercase'>Seconds to date</MDBCardTitle>
-                            <br />
-                            <MDBCardText>
-                                Convert seconds to date( years, months, days...)
-                            </MDBCardText>
-                            <MDBCardLink href='/calculator/seconds-to-date'>Enter</MDBCardLink>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-                <MDBCol md='4'>
-                    <MDBCard >
-                        <MDBCardBody>
-                            <MDBCardTitle className='text-uppercase'>Multiplying large numbers</MDBCardTitle>
-                            <br />
-                            <MDBCardText>
-                                Multiplying very large numbers.
-                            </MDBCardText>
-                            <MDBCardLink href='/calculator/large-numbers'>Enter</MDBCardLink>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-                <MDBCol md='4'>
-                    <MDBCard >
-                        <MDBCardBody>
-                            <MDBCardTitle className='text-uppercase'>Tribonacci</MDBCardTitle>
-                            <br />
-                            <MDBCardText>
-                                Find tribonacci numbers, between two points.
-                            </MDBCardText>
-                            <MDBCardLink href='/calculator/tribonacci'>Enter</MDBCardLink>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-                <MDBCol md='4' className='mt-md-5'>
-                    <MDBCard >
-                        <MDBCardBody>
-                            <MDBCardTitle className='text-uppercase'>Age in days</MDBCardTitle>
-                            <br />
-                            <MDBCardText>
-                                Did you ever want to know how many days old are you? Here you can find your age in days, hours ....
-                            </MDBCardText>
-                            <MDBCardLink href='/calculator/age-in-days'>Enter</MDBCardLink>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-            </MDBRow>
-        </MDBContainer>
+        <section className='bgc-lightblue'>
+            <MDBContainer className='pb-md-5'>
+                <MDBRow >
+                    <MDBCardTitle className='text-center my-md-5 display-4'>The Calculators</MDBCardTitle>
+                    <MDBCol md='4'>
+                        <MDBCard >
+                            <MDBCardBody>
+                                <MDBCardTitle className='text-uppercase'>Seconds to date</MDBCardTitle>
+                                <br />
+                                <MDBCardText>
+                                    Convert seconds to date( years, months, days...)
+                                </MDBCardText>
+                                <MDBCardLink href='/calculator/seconds-to-date'>Enter</MDBCardLink>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol md='4'>
+                        <MDBCard >
+                            <MDBCardBody>
+                                <MDBCardTitle className='text-uppercase'>Multiplying large numbers</MDBCardTitle>
+                                <br />
+                                <MDBCardText>
+                                    Multiplying very large numbers.
+                                </MDBCardText>
+                                <MDBCardLink href='/calculator/large-numbers'>Enter</MDBCardLink>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol md='4'>
+                        <MDBCard >
+                            <MDBCardBody>
+                                <MDBCardTitle className='text-uppercase'>Tribonacci</MDBCardTitle>
+                                <br />
+                                <MDBCardText>
+                                    Find tribonacci numbers, between two points.
+                                </MDBCardText>
+                                <MDBCardLink href='/calculator/tribonacci'>Enter</MDBCardLink>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                    <MDBCol md='4' className='mt-md-5'>
+                        <MDBCard >
+                            <MDBCardBody>
+                                <MDBCardTitle className='text-uppercase'>Age in days</MDBCardTitle>
+                                <br />
+                                <MDBCardText>
+                                    Did you ever want to know how many days old are you? Here you can find your age in days, hours ....
+                                </MDBCardText>
+                                <MDBCardLink href='/calculator/age-in-days'>Enter</MDBCardLink>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        </section>
     );
 }
--- src/views/Games.jsx
+++ src/views/Games.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple, MDBContainer, MDBRow, MDBCol, MDBCardHeader } from 'mdb-react-ui-kit';
-
+import "../assets/styles/GeneralBGC.css"
 export default function Jeux() {
+
     return (
-        <MDBContainer className='pb-md-5'>
-            <MDBRow >
-                <MDBCardTitle className='text-center my-md-5 display-4'>The Games</MDBCardTitle>
-                <MDBCol md='4'>
-                    <MDBCard >
-                        <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
-                            <MDBCardImage src='https://www.quedujouet.fr/5126-large_default/jeu-des-petits-chevaux-en-bois-format-voyage.jpg' fluid alt='...' />
-                            <a>
-                                <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
-                            </a>
-                        </MDBRipple>
-                        <MDBCardBody>
-                            <MDBCardTitle className='text-uppercase'>Jeu des petits chevaux</MDBCardTitle>
-                            <MDBCardText className='fw-lighter text-justify'>
-                                Le jeu des petits chevaux est un jeu qui consiste à déplacer plusieurs pions par joueur, et à les emmener sur la partie réservée à leur couleur. Le premier joueur qui arrive sur la dernière case triangulaire remporte la partie.
-                            </MDBCardText>
-                            <MDBBtn href='/games/petits-chevaux'>Enter</MDBBtn>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-            </MDBRow>
-        </MDBContainer>
+        <section className='bgc-lightblue'>
+            <MDBContainer className='pb-md-5 bgc-lightblue'>
+                <MDBRow >
+                    <MDBCardTitle className='text-center my-md-5 display-4'>The Games</MDBCardTitle>
+                    <MDBCol md='4'>
+                        <MDBCard >
+                            <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+                                <MDBCardImage src='https://www.quedujouet.fr/5126-large_default/jeu-des-petits-chevaux-en-bois-format-voyage.jpg' fluid alt='...' />
+                                <a>
+                                    <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+                                </a>
+                            </MDBRipple>
+                            <MDBCardBody>
+                                <MDBCardTitle className='text-uppercase'>Jeu des petits chevaux</MDBCardTitle>
+                                <MDBCardText className='fw-lighter text-justify'>
+                                    Le jeu des petits chevaux est un jeu qui consiste à déplacer plusieurs pions par joueur, et à les emmener sur la partie réservée à leur couleur. Le premier joueur qui arrive sur la dernière case triangulaire remporte la partie.
+                                </MDBCardText>
+                                <MDBBtn href='/games/petits-chevaux'>Enter</MDBBtn>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        </section>
     );
 }
