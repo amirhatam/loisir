f3b12f1c16d0c1c1328a328f9015c3a9771aadd0
Amir <amirhatam136@gmail.com>
2022-04-04 23:02:58 +0200

    add responsive styles for page calculators

--- src/views/Calculators.jsx
+++ src/views/Calculators.jsx
@@ -5,62 +5,62 @@ export default function Calculator() {
     return (
         <section className='bgc-lightblue'>
             <MDBContainer className='pb-md-5'>
-                <MDBRow >
-                    <MDBCardTitle className='text-center my-md-5 display-5'>The Calculators</MDBCardTitle>
-                    <MDBCol md='4'>
+                <MDBRow className='justify-content-center'>
+                    <MDBCardTitle className='text-center my-md-5 my-4 display-5'>The Calculators</MDBCardTitle>
+                    <MDBCol className='col-md-4 col-9 my-3'>
                         <MDBCard >
                             <MDBCardBody>
-                                <MDBCardTitle className='text-uppercase'>Seconds to date</MDBCardTitle>
+                                <MDBCardTitle className='text-uppercase h6-fs'>Seconds to date</MDBCardTitle>
                                 <br />
-                                <MDBCardText>
+                                <MDBCardText className='p-fs'>
                                     Convert seconds to date( years, months, days...)
                                 </MDBCardText>
                                 <MDBCardLink href='/calculator/seconds-to-date'>Enter</MDBCardLink>
                             </MDBCardBody>
                         </MDBCard>
                     </MDBCol>
-                    <MDBCol md='4'>
+                    <MDBCol className='col-md-4 col-9 my-3'>
                         <MDBCard >
                             <MDBCardBody>
-                                <MDBCardTitle className='text-uppercase'>Multiplying large numbers</MDBCardTitle>
+                                <MDBCardTitle className='text-uppercase h6-fs'>Multiplying large numbers</MDBCardTitle>
                                 <br />
-                                <MDBCardText>
+                                <MDBCardText className='p-fs'>
                                     Multiplying very large numbers.
                                 </MDBCardText>
                                 <MDBCardLink href='/calculator/large-numbers'>Enter</MDBCardLink>
                             </MDBCardBody>
                         </MDBCard>
                     </MDBCol>
-                    <MDBCol md='4'>
+                    <MDBCol className='col-md-4 col-9 my-3'>
                         <MDBCard >
                             <MDBCardBody>
-                                <MDBCardTitle className='text-uppercase'>Tribonacci</MDBCardTitle>
+                                <MDBCardTitle className='text-uppercase h6-fs'>Tribonacci</MDBCardTitle>
                                 <br />
-                                <MDBCardText>
+                                <MDBCardText className='p-fs'>
                                     Find tribonacci numbers, between two numbers.
                                 </MDBCardText>
                                 <MDBCardLink href='/calculator/tribonacci'>Enter</MDBCardLink>
                             </MDBCardBody>
                         </MDBCard>
                     </MDBCol>
-                    <MDBCol md='4' className='mt-md-5'>
+                    <MDBCol className='col-md-4 col-9 my-3'>
                         <MDBCard >
                             <MDBCardBody>
-                                <MDBCardTitle className='text-uppercase'>Age in days</MDBCardTitle>
+                                <MDBCardTitle className='text-uppercase h6-fs'>Age in days</MDBCardTitle>
                                 <br />
-                                <MDBCardText>
+                                <MDBCardText className='p-fs'>
                                     find your age in days, hours, minutes, seconds
                                 </MDBCardText>
                                 <MDBCardLink href='/calculator/age-in-days'>Enter</MDBCardLink>
                             </MDBCardBody>
                         </MDBCard>
                     </MDBCol>
-                    <MDBCol md='4' className='mt-md-5'>
+                    <MDBCol className='col-md-4 col-9 my-3'>
                         <MDBCard >
                             <MDBCardBody>
-                                <MDBCardTitle className='text-uppercase'>Square number</MDBCardTitle>
+                                <MDBCardTitle className='text-uppercase h6-fs'>Square number</MDBCardTitle>
                                 <br />
-                                <MDBCardText className=''>
+                                <MDBCardText className='p-fs'>
                                     Find square numbers, between two numbers.
                                 </MDBCardText>
                                 <MDBCardLink href='/calculator/square-numbers'>Enter</MDBCardLink>
