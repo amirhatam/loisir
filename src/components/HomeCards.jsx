ce63f91a96172cee5082e4d134effa90f34d6e05
Amir <amirhatam136@gmail.com>
2022-05-17 18:30:45 +0200

    DRY code for HomeCard

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,108 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MDBCard, MDBCardTitle, MDBCardBody, MDBCardHeader, MDBRow, MDBCol, MDBCardLink, Link } from 'mdb-react-ui-kit';
 import "../assets/styles/HomeCards.css";
 
 
 export default function HomeCards() {
+    // const [colors] = useState(['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'dark'])
+    const [cards] = useState([
+        {
+            category: 'The Game',
+            title: 'PETITS CHEVAUX',
+            boxColor: 'primary',
+            url: '/games'
+        },
+        {
+            category: 'The Game',
+            title: 'HANGMAN',
+            boxColor: 'secondary',
+            url: '/games'
+        },
+        {
+            category: 'The Calculator',
+            title: 'SECONDS TO DATE',
+            boxColor: 'success',
+            url: '/calculator'
+        },
+        {
+            category: 'The Calculator',
+            title: 'MULTIPLY BIG NUMBERS',
+            boxColor: 'danger',
+            url: '/calculator'
+        },
+        {
+            category: 'The Calculator',
+            title: 'TRIBONACCI NUMBERS',
+            boxColor: 'warning',
+            url: '/calculator'
+        },
+        {
+            category: 'The Calculator',
+            title: 'AGE IN DAYS',
+            boxColor: 'info',
+            url: '/calculator'
+        },
+        {
+            category: 'The Calculator',
+            title: 'SQUARE NUMBER',
+            boxColor: 'dark',
+            url: '/calculator'
+        },
+        {
+            category: 'The Calculator',
+            title: 'Volume of a Cuboid',
+            boxColor: 'primary',
+            url: '/calculator'
+        },
+    ])
+
     return (
         <MDBRow className='justify-content-center my-5'>
-            <MDBCol className='col-md-3 col-6'>
-                <MDBCard background='primary' className='text-white mb-3 card-container' >
-                    <MDBCardHeader>
-                        <MDBCardLink href='/games' className='txt-white p-fs'>
-                            The Game
-                        </MDBCardLink>
-                    </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardTitle className='h6-fs'>PETITS CHEVAUX</MDBCardTitle>
-                    </MDBCardBody>
-                </MDBCard>
-            </MDBCol>
-
-            <MDBCol className='col-md-3 col-6'>
-                <MDBCard background='secondary' className='text-white mb-3 card-container' >
-                    <MDBCardHeader>
-                        <MDBCardLink href='/games' className='txt-white p-fs'>
-                            The Game
-                        </MDBCardLink>
-
-                    </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardTitle className='h6-fs'>HANGMAN</MDBCardTitle>
-                    </MDBCardBody>
-                </MDBCard>
-            </MDBCol>
-
-            <MDBCol className='col-md-3 col-6'>
-                <MDBCard background='success' className='text-white mb-3 card-container' >
-                    <MDBCardHeader >
-                        <MDBCardLink href='/calculator' className='txt-white p-fs'>
-                            The Calculator
-                        </MDBCardLink>
-                    </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardTitle className='h6-fs'>SECONDS TO DATE</MDBCardTitle>
-
-                    </MDBCardBody>
-                </MDBCard>
-            </MDBCol>
-
-            <MDBCol className='col-md-3 col-6'>
-                <MDBCard background='danger' className='text-white mb-3 card-container' >
-                    <MDBCardHeader>
-                        <MDBCardLink href='/calculator' className='txt-white p-fs'>
-                            The Calculator
-                        </MDBCardLink>
-                    </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardTitle className='h6-fs'>MULTIPLY BIG NUMBERS</MDBCardTitle>
-
-                    </MDBCardBody>
-                </MDBCard>
-            </MDBCol>
-
-            <MDBCol className='col-md-3 col-6'>
-                <MDBCard background='warning' className='text-white mb-3 card-container' >
-                    <MDBCardHeader>
-                        <MDBCardLink href='/calculator' className='txt-white p-fs'>
-                            The Calculator
-                        </MDBCardLink>
-                    </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardTitle className='h6-fs'>TRIBONACCI NUMBERS</MDBCardTitle>
-
-                    </MDBCardBody>
-                </MDBCard>
-            </MDBCol>
-
-            <MDBCol className='col-md-3 col-6'>
-                <MDBCard background='info' className='text-white mb-3 card-container' >
-                    <MDBCardHeader>
-                        <MDBCardLink href='/calculator' className='txt-white p-fs'>
-                            The Calculator
-                        </MDBCardLink>
-                    </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardTitle className='h6-fs'>AGE IN DAYS</MDBCardTitle>
-                    </MDBCardBody>
-                </MDBCard>
-            </MDBCol>
-
-            <MDBCol className='col-md-3 col-6'>
-                <MDBCard background='dark' className='text-white card-container' >
-                    <MDBCardHeader className='border-bottom'>
-                        <MDBCardLink href='/calculator' className='txt-white p-fs'>
-                            The Calculator
-                        </MDBCardLink>
-                    </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardTitle className='h6-fs'>SQUARE NUMBER</MDBCardTitle>
-                    </MDBCardBody>
-                </MDBCard>
-            </MDBCol>
 
 
 
+            {
+                cards.map((e, i) => {
+                    return (
+                        <MDBCol key={i} className='col-md-3 col-6'>
+                            <MDBCard background={e.boxColor} className='text-white mb-3 card-container' >
+                                <MDBCardHeader>
+                                    <MDBCardLink href={e.url} className='txt-white p-fs'>
+                                        {e.category}
+                                    </MDBCardLink>
+                                </MDBCardHeader>
+                                <MDBCardBody>
+                                    <MDBCardTitle className='h6-fs'>{e.title}</MDBCardTitle>
+                                </MDBCardBody>
+                            </MDBCard>
+                        </MDBCol>
+                    )
+                })
+            }
         </MDBRow>
     );
 }
