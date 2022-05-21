b31a9c696ef2b4c75c6f811412e1c1db52fcfe84
Amir <amirhatam136@gmail.com>
2022-05-21 08:50:53 +0200

    git push to deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export const Footer = () => {
 
     return (
-
         <footer className="bg-dark text-center text-white">
             <div className="container p-4 pb-0">
                 <section className="mb-4">
